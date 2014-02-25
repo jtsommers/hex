a27341b92a4b2d23b0cbb543253c42ce8e4c6ad3
Matthew Balousek <matthew@balousek.net>
2014-02-25 13:24:31 -0800

    Xcode SCM test.

---
+++ CMPS 109 Hex/CMPS 109 Hex/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  CMPS 109 Hex
+//
+//  Created by mrfb on 2/25/14.
+//  Copyright (c) 2014 CMPS109_W14. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+	// insert code here...
+	std::cout << "Hello, Hex!\n";
+    return 0;
+}
+
